21be40870301b17bddb6c1a3b7e75afe32f2d8bc
Kailash Maurya <97893807+Kailash51@users.noreply.github.com>
2023-12-25 11:52:26 +0530

    Time: 9 ms (5.46%), Space: 16.1 MB (29.96%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+ void possibleSubsetsHelper(vector<int>& arr,int index, vector<int> subset,vector<vector<int>> &powerset, int arraySize){
+        if(index == arraySize) return powerset.push_back(subset);
+        
+        
+        // pick
+        subset.push_back(arr[index]);
+        possibleSubsetsHelper(arr,index+1,subset,powerset,arraySize);
+        subset.pop_back();
+        //notpick
+        // while(index+1<arraySize && arr[index] == arr[index+1]){
+        //     index++;
+        // }
+        possibleSubsetsHelper(arr,index+1,subset,powerset,arraySize);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+         vector<vector<int>> powerset;
+        vector<int> subset;
+        int arraySize = nums.size();
+        sort(nums.begin(), nums.end());
+        possibleSubsetsHelper(nums,0,subset,powerset,arraySize);
+        return powerset;
+    }
+};
